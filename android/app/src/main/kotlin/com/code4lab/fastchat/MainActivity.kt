cc04259bc86993c0b6fa2d71cf117649f2f0313b
satyam <sat.tiwari21@gmail.com>
2020-01-13 21:08:25 +0530

    First Commit

---
+++ android/app/src/main/kotlin/com/code4lab/fastchat/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.code4lab.fastchat
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
